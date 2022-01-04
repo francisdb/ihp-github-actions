8cd4f8c9c3caf2340abdacccccae2ce758c90ad9
Francis De Brabandere <francisdb@gmail.com>
2022-01-04 14:45:12 +0100

    Add tests

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -1,4 +1,6 @@
 -- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
-CREATE TABLE customers (
-    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL
+CREATE TABLE posts (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL
 );
--- Web/FrontController.hs
+++ Web/FrontController.hs
@@ -5,12 +5,16 @@ import Web.Controller.Prelude
 import Web.View.Layout (defaultLayout)
 
 -- Controller Imports
+import Web.Controller.Posts
+import Web.Controller.Posts
 import Web.Controller.Static
 
 instance FrontController WebApplication where
     controllers = 
         [ startPage WelcomeAction
         -- Generator Marker
+        , parseRoute @PostsController
+        , parseRoute @PostsController
         ]
 
 instance InitControllerContext WebApplication where
